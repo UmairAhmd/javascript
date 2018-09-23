d383e0ad8fd74cea55a5f8c29bd5d1ee59ab06d6
Umair Bin Ahmad <umairnadeem20@hotmail.com>
2018-09-23 15:48:54 +0500

    second commit

---
+++ function_declaration.js
@@ -0,0 +1,39 @@
+/*
+function sayHi()
+{
+    console.log("Hi, I'm a function");
+}
+*/
+
+//sayHi();
+
+/*
+let a =sayHi;
+a();
+console.log(typeof a);
+*/
+
+/*
+function greetings(Name)
+{
+    console.log("Hi, Mr. ",Name);
+}
+
+greetings("Umair");
+*/
+
+/*
+function calculateSum(a,b)
+{
+    return a+b;
+}
+*/
+
+
+
+// let output=calculateSum(2,3);
+// console.log(output);
+// console.log(calculateSum(2,2));
+
+// let a=calculateSum;
+// console.log(a(20,10));
---
+++ iteration.js
@@ -0,0 +1,43 @@
+// ----------------- for loop ----------------
+/*
+for(i=0;i<10;i++)
+{
+    console.log(a);
+}
+*/
+
+/*
+let a=[3,4,15,6,17];
+for(i=0;i<a.length;i++)
+{
+    console.log(a[i]);
+}
+*/
+
+// ------- x++ vs ++x  in while loop -----------
+
+/*
+let x=1;
+while(x<10)
+{
+    console.log(++x);
+}
+
+x=1;
+while(x<10)
+{
+    console.log(x++);
+}
+*/
+
+/*
+let a=200;
+while(a<500)
+{
+    console.log(++a);
+    if(a==300)
+    {
+        break;
+    }
+}
+*/
