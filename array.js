74db785849e88b5462bfb9b7aa29b5f5f93689a5
Umair Bin Ahmad <umairnadeem20@hotmail.com>
2018-09-23 15:46:02 +0500

    first commit

---
+++ array.js
@@ -0,0 +1,31 @@
+let a=[5,10,15,20,25];
+let days=['Monday','Tuesday','Wednesday'];
+
+let c=[1,2,"hi","bye",3];
+
+/*
+console.log(a[2]);
+console.log(c[2]);
+
+console.log(typeof c);
+*/
+
+/*
+console.log(c.length);
+
+a[5]=1;
+console.log(a);
+
+days[6]="Sunday";
+console.log(days);
+
+console.log(days.length);   
+*/
+
+/*
+c.push("last");
+console.log(c);
+
+c.pop();
+console.log(c);
+*/
